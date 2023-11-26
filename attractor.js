cf1f8aaea7650bafe2682bd878ea026edad31334
Jay-Cha <147472646+jay010906@users.noreply.github.com>
2023-11-26 14:56:02 +0900

    어트랙터의 이동 및 power 값 변경
    어트랙터를 x축으로 움직이게 하였고 power도 늘려줌

--- attractor.js
+++ attractor.js
@@ -1,27 +1,23 @@
 class Attractor {
   constructor(x, y) {
     this.position = createVector(x, y);
-    this.power = 100;
+    this.power = 300;
     this.value = 0.8;
   }
   
   move() {
-    // this.position.x -= this.value;
+  this.position.x -= this.value;
 
-  // if(this.position.x > width || this.position.x < 0){
-  //   this.value *= -1;
-  //  }
+  if(this.position.x > width || this.position.x < 0){
+    this.value *= -1;
+   }
   }
   
-  setPower(value) {
-    this.power = map(value, 0, width, -300, 300);
-  }
-
   show() {
     stroke(0);
     noStroke();
     fill(0);
-    circle(this.position.x, this.position.y, 1);
+    circle(this.position.x, this.position.y, 30);
   }
 
   pull(particle) {
